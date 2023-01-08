e5512c9e5eb9974586d8f1600815520994b4d600
Cha-Shao <yrchashao@outlook.com>
2023-01-08 23:59:24 +0800

    登录

--- src/api/checkLogin.ts
+++ src/api/checkLogin.ts
@@ -1,17 +1,18 @@
 import axios from 'axios'
 import type { Ref } from 'vue'
 import { ref } from 'vue'
-import { get_cookie } from '../utils/getCookie'
 
 export const loginStatus: Ref<any> = ref({ msg: '登录中', username: null, status: null })
 
 const checkLogin = () => {
-  const token = get_cookie('token')
+  const token = localStorage.getItem('token')
 
   // 登录信息存在
   if (token) {
     axios
-      .post('https://sso.elfmc.com/account/get_info', {}, {
+      .post('https://sso.elfmc.com/account/get_info', {
+        token,
+      }, {
         withCredentials: true,
       })
       .then((response) => {
@@ -20,6 +21,8 @@ const checkLogin = () => {
       .catch((error) => {
         console.error(error)
         loginStatus.value = error.response.data
+        if (error.response.status === 401)
+          localStorage.removeItem('token')
       })
   }
   else {
